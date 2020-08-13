4cefbb6f5eaba42430cd9d26be817f06228c082a
Julius <juliushussl@gmail.com>
2020-08-13 14:10:45 +0200

    Basic Session Enviroment

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,82 @@
+import { SessionEnviroment } from "./session-class";
 import * as express from "express";
 import { createServer } from "http";
 import * as socketio from "socket.io";
 
-const sockets = new Map<string, SessionData>();
 const sessionsStore = new Map<string, SessionEnviroment>();
+var cors = require("cors");
 const app = express();
 const http = createServer(app);
-const rootSocket = socketio(http);
 
-app.get("/", (req, res) => {
+app.use(cors());
+/*app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-});
+});*/
 
 let id: number = 1000;
 
-function getNewId(): string {
-  return String(getRandomInt(1000, 9999));
-}
+function getNewSessionId(digits: number) {
+  let generatedId: string = getRandomId(4, true, true, false);
+  while (sessionsStore.has(generatedId)) {
+    generatedId = getRandomId(4, true, true, false);
+  }
 
-export interface ChatData {
-  chatId: string;
-  chatUser?: number;
-  chatMessages: ChatMessage[];
-}
-
-export interface ChatMessage {
-  userId?: number;
-  date?: Date;
-  message: string;
-  messageId: number;
-}
-
-export interface SessionData {
-  socket: any;
-  chatData?: ChatData;
-}
-
-function getRandomInt(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return generatedId;
 }
 
 app.post("/new", (req, res) => {
-  const newId = getNewId();
+  const newId = getNewSessionId(4);
 
   const io = socketio(http, {
     path: `/${newId}`,
   });
 
-  const sessionEnviroment: SessionEnviroment = new SessionEnviroment(newId);
+  const sessionEnviroment: SessionEnviroment = new SessionEnviroment(newId, io);
   sessionsStore.set(newId, sessionEnviroment);
+  console.log(sessionsStore);
+  console.log("New Session generated");
 
   io.on("connection", (socket) => {
     socket.join(newId);
     console.log("Connected");
-    let chatData: ChatData = {
-      chatId: newId,
-      chatMessages: [],
-    };
-    socket.on("chat message", (msg) => {
-      const chatMessage: ChatMessage = {
-        message: msg,
-        messageId: 404,
-      };
-      chatData.chatMessages.push(chatMessage);
-      console.log(newId);
 
-      io.in(newId).emit("chat message", msg);
+    io.in(socket.id).emit("chat message", "successfully connected to " + newId);
+    sessionEnviroment.registerUser(socket.id);
+
+    socket.on("chat message", (msg) => {
+      sessionEnviroment.sendChatMessage(msg);
     });
 
     socket.on("disconnect", () => {
+      sessionEnviroment.disconnectUser();
       console.log("user disconnected");
     });
-
-    io.in(socket.id).emit("chat message", "successfully connected to " + newId);
   });
-  const session: SessionData = {
-    socket: io,
-  };
-  sockets.set(newId, session);
-  console.log(sockets);
   res.json({ sessionId: newId });
 });
 
 http.listen(3000, () => {
   console.log("listening on *:3000");
 });
+
+function getRandomId(
+  digits: number,
+  numbers: boolean,
+  capitalLetter: boolean,
+  letter: boolean
+): string {
+  const nChar = "0123456789";
+  const cChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lChar = "abcdefghijklmnopqrstuvwxyz";
+
+  var characters = numbers ? nChar : "";
+  characters += capitalLetter ? cChar : "";
+  characters += letter ? lChar : "";
+
+  var result = "";
+  var charactersLength = characters.length;
+  for (let i: number = 0; i < digits; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
--- src/session-class.ts
+++ src/session-class.ts
@@ -1,8 +1,46 @@
-class SessionEnviroment {
+export interface ChatData {
+  chatId: string;
+  chatUser?: number;
+  chatMessages: ChatMessage[];
+}
+
+export interface ChatMessage {
+  userId?: number;
+  date?: Date;
+  message: string;
+  messageId: number;
+}
+
+export class SessionEnviroment {
   id: string;
-  constructor(newId: string) {
+  chatData: ChatData;
+  io;
+
+  constructor(newId: string, newSocket: any) {
     this.id = newId;
+    this.io = newSocket;
+    this.chatData = {
+      chatId: this.id,
+      chatMessages: [],
+    };
   }
 
-  sendChatMessage() {}
+  sendChatMessage(message: string) {
+    console.log(message);
+    const chatMessage: ChatMessage = {
+      message: message,
+      messageId: 404,
+    };
+    this.chatData.chatMessages.push(chatMessage);
+    this.io.in(this.id).emit("chat message", message);
+  }
+
+  registerUser(UserId: string) {
+    for (const msg of this.chatData.chatMessages) {
+      this.io.in(UserId).emit("chat message", msg.message);
+    }
+    console.log("User:" + UserId + " joined on session " + this.id);
+  }
+
+  disconnectUser() {}
 }
